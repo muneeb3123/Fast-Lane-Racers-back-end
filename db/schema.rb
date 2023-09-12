7ee27b25d1766fbfd030ed237cf03fa40894cce1
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-09-12 11:31:48 +0500

    Create Car model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_11_045326) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_061923) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "finance_fee"
+    t.integer "option_to_purchase_fee"
+    t.integer "total_amount_payable"
+    t.string "duration"
+    t.decimal "apr"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
