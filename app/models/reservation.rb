39e8f00619faa9cc61dd49333a040126a5ff7398
abiola-arobieke <abiola.arobieke@gmail.com>
2023-09-12 16:27:28 +0100

    Add valiations to reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,10 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :car
+
+  attribute :city, :string
+  attribute :date, :date
+
+  validates :city, presence: true
+  validates :date, presence: true, availability: true
 end
