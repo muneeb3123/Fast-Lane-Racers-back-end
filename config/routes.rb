3494e31b9c4e6eae1313f9b0be8b8c2f1492a73a
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-09-12 14:21:56 +0500

    Create car controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/current_user', to: 'current_user#index'
+  
+  resources :cars, only: [:index, :show, :new, :create, :destroy]
 
   devise_for :users, path:'', path_names: {
     sign_in: 'login',
