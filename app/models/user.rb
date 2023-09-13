4373e0e57bc3b026801a7a4f2d8d0de5834dc4cb
abiola-arobieke <abiola.arobieke@gmail.com>
2023-09-13 07:28:11 +0100

    Modify users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :reservations
+
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable, :validatable,
