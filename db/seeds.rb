dea599e1eac822f0d699db99038205a7a239da13
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-09-14 07:03:26 +0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cars = [
+  ['Ford', 'Fantastic Car', 11, 21, 71, '31 months', 4.1, 'https://placekitten.com/600/600'],
+  ['BMW', 'Brilliant Car', 12, 22, 72, '32 months', 4.2, 'https://placekitten.com/600/600'],
+  ['Rolls Royce', 'Resplendent car', 13, 23, 73, '33 months', 4.3, 'https://placekitten.com/600/600']
+]
+
+cars.each do |car|
+  Car.create(name: car[0], description: car[1], finance_fee: car[2], option_to_purchase_fee: car[3], total_amount_payable: car[4], duration: car[5], apr: car[6], image: car[7])
+end
