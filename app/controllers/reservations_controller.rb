dbb089ae9fa14f4f488187adb9b50e8a2edc3b8f
abiola-arobieke <abiola.arobieke@gmail.com>
2023-09-14 07:21:54 +0100

    Update reservation controller

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,21 +1,26 @@
 class ReservationsController < ApplicationController
+    before_action :authenticate_user!
+
     def index
-        reservation = current_user.reservations
-        render json: reservation
+        @reservation = current_user.reservations
+        render json: @reservation
     end
 
     def create
-        user = User.find(params[:user_id])
-        car_id = Car.find(params[:car_id])
-        @reserve = Reservation.new(reservation_params)
+        @reserve = current_user.reservations.new(reservation_params)
         if @reserve.save
             render json: { response: "Successfully added to reservation" }
         else
             render json: @reserve.errors, status: :unprocessable_entity
         end
     end
+
+    def show
+    end
     
+    private
+
     def reservation_params
-        params.require(:reservation).permit(:user_id, :car_id, :city, :date)
+        params.require(:reservation).permit(:car_id, :city, :date)
     end
 end
