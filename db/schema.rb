c18c5f8e968faf0e7f67f5e7bd3dd30757d5ae02
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-09-14 15:14:26 +0500

    modify

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_09_12_061923) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_12_144503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -27,7 +26,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_12_061923) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "reservations", force: :cascade do |t|
     t.bigint "user_id", null: false
