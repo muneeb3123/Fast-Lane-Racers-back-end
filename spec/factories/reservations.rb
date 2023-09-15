70e314044987c0e4002609832c3cf59c8441cda7
Ashnaali3255 <ashnaalipk@gmail.com>
2023-09-15 05:36:16 +0500

    Fix reservation linters

--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
-    factory :reservation do
-      association :user, factory: :user
-      association :car, factory: :car
-      city { Faker::Address.city }
-      date { Faker::Date.forward(days: 30) } 
-    end
+  factory :reservation do
+    association :user, factory: :user
+    association :car, factory: :car
+    city { Faker::Address.city }
+    date { Faker::Date.forward(days: 30) }
   end
-  
+end
