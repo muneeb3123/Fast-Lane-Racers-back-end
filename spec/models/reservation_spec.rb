66a21bb4bfef052ba374d9c2cbf351afa84c504a
Ashnaali3255 <ashnaalipk@gmail.com>
2023-09-15 05:36:16 +0500

    Fix reservation linters

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,34 +1,3 @@
-# require 'rails_helper'
-
-# RSpec.describe Reservation, type: :model do
-#   describe 'associations' do
-#     it { should belong_to(:user) }
-#     it { should belong_to(:car) }
-#   end
-
-#   describe 'validations' do
-#     it { should validate_presence_of(:city) }
-#     it { should validate_presence_of(:date) }
-
-#     it 'validates date availability' do
-#       user = create(:user)
-#       car = create(:car)
-#       valid_date = Date.tomorrow
-#       invalid_date = Date.yesterday
-
-#       # Create a valid reservation
-#       valid_reservation = build(:reservation, user: user, car: car, date: valid_date)
-#       expect(valid_reservation).to be_valid
-
-#       # Create an invalid reservation with a past date
-#       invalid_reservation = build(:reservation, user: user, car: car, date: invalid_date)
-#       expect(invalid_reservation).not_to be_valid
-#     end
-#   end
-# end
-
-# spec/models/reservation_spec.rb
-
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
