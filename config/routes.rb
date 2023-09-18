325054d40a25b067f732994acf9657f8e3e91d57
abiola-arobieke <abiola.arobieke@gmail.com>
2023-09-18 15:24:04 +0100

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :reservations, only: [:index, :show, :create]
+  resources :reservations, only: [:index, :create]
 
   get '/current_user', to: 'current_user#index'
   
