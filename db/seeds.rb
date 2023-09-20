7490025922465d455917b20aba2d7b074f557761
sameedmohsin28 <sameedmohsin22@hotmail.com>
2023-09-21 00:24:42 +0500

    Update seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,10 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 cars = [
-  ['Mercedes', '
-    Mercedes is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 11, 21, 71, '31 months', 4.1, 'https://i.imgur.com/h9Mo1AZ.png', 'Black'],
-  ['McLaren P1', '
-    McLaren P1 is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 12, 22, 72, '32 months', 4.2, 'https://i.imgur.com/o1OMj06.png', 'Grey'],
-  ['Bugatti Veyron P1.6', '
-    Bugatti Veyron P1.6 is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 13, 23, 73, '33 months', 4.3, 'https://i.imgur.com/ahilqIn.png', 'Red'],
-  ['Ferrari 599 SA Aperta', '
-    Ferrari 599 SA Aperta is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 14, 24, 74, '34 months', 4.4, 'https://i.imgur.com/L9XaS2r.png', 'White']
+  ['Mercedes-Maybach S650 Cabriolet', 'Mercedes-Maybach S650 Cabriolet is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 11, 21, 71, '31 months', 4.1, 'https://i.imgur.com/h9Mo1AZ.png', 'Black'],
+  ['McLaren P1', 'McLaren P1 is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 12, 22, 72, '32 months', 4.2, 'https://i.imgur.com/o1OMj06.png', 'Grey'],
+  ['Bugatti Veyron P1.6', 'Bugatti Veyron P1.6 is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 13, 23, 73, '33 months', 4.3, 'https://i.imgur.com/ahilqIn.png', 'Red'],
+  ['Ferrari 599 SA Aperta', 'Ferrari 599 SA Aperta is an iconic Italian sports car brand celebrated for its sleek and powerful vehicles. It represents the pinnacle of automotive excellence and performance.', 14, 24, 74, '34 months', 4.4, 'https://i.imgur.com/L9XaS2r.png', 'White']
 ]
 
 cars.each do |car|
@@ -24,3 +20,6 @@ end
 
 # Create Admin user
 User.create(name: "muneeb", email: "muneeb@gmail.com", password:"123456", password_confirmation: "123456", role: "admin")
+
+Reservation.create(user_id: 1, car_id: 1, city: 'Melbourne', date: 'Tue, 26 Sep 2023');
+Reservation.create(user_id: 1, car_id: 2, city: 'Auckland', date: 'Wed, 27 Sep 2023');
