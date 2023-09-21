09592722a4d0041f72c2f258dee088ad9ad814bc
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-09-21 16:56:21 +0500

    update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'example.com', 'localhost:3000'  
+    origins '*'  
 
     resource '*',
       headers: :any,
