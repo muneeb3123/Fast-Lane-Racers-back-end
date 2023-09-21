314920f7a8ab8f60698b60523ebfd463a1c6010a
Ashnaali3255 <ashnaalipk@gmail.com>
2023-09-22 01:13:22 +0500

    Update cors.rb for api doc

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  
+    origins 'example.com', 'localhost:3000'  
 
     resource '*',
       headers: :any,
