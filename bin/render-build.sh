b3ab7be3f03645ac6674f6f4b2ab57218217095f
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-01-19 01:09:04 +0500

    update

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 # exit on error
+# set -o errexit
+
+# bundle install
+# bundle exec rake db:seed
+# bundle exec rake db:migrate
+
 set -o errexit
 
 bundle install
-bundle exec rake db:seed
-bundle exec rake db:migrate
+./bin/rails assets:precompile
+./bin/rails assets:clean
